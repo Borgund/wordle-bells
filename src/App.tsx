e0da0923b859ce1cea66ef9bbd4e37510583bbad
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2022-11-22 15:42:29 +0100

    Removed more random stuff 🤷‍♂️

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import CustomCountdown from "./components/countdown";
 import FancyDay from "./components/fancyDay";
 
 function App() {
-  const [count, setCount] = useState(0);
   const today = new Date();
   const christmas = new Date("2022-12-24");
   const isBeforeChristmas = +today < +christmas;
@@ -18,29 +17,25 @@ function App() {
       <div>
         <img src="/vite.svg" className="logo" alt="Vite logo" />
         <img src={reactLogo} className="logo react" alt="React logo" />
-        <img src={dnbLogo} className="logo react" alt="React logo" />
+        <img src={dnbLogo} className="logo" alt="DNB logo" />
       </div>
       <h1>Vite + React + DNB + Wordle = ❤️</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> to get christmas cracking! 🎅🏻
-        </p>
-        {isBeforeChristmas && (
-          <>
-            <p>Today is:</p>
-            <FancyDay date={today} />
-            <p>So it's chrismas in:</p>
-          </>
-        )}
-        <p>
-          <CustomCountdown date={christmas}>
-            <p>It's Christmas baby! Go do something else! 🎅🏻</p>
-          </CustomCountdown>
-        </p>
-      </div>
+      <p>
+        Edit <code>src/App.tsx</code> to get christmas cracking! 🎅🏻
+      </p>
+      {isBeforeChristmas && (
+        <>
+          <p>Today is:</p>
+          <FancyDay date={today} />
+          <p>So it's chrismas in just:</p>
+        </>
+      )}
+      <p>
+        <CustomCountdown date={christmas}>
+          <p>It's Christmas baby! Go do something else! 🎅🏻</p>
+        </CustomCountdown>
+      </p>
+
       <Snowfall style={{ zIndex: "-100" }} snowflakeCount={40} />
     </div>
   );
