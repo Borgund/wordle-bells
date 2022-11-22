4f1bec01255ea0058e09c1824c308c8eb70cbe44
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2022-11-22 10:56:35 +0100

    DNBed stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import dnbLogo from "./assets/dnb-logo.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={dnbLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + DNB + Wordle = ❤️</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> to get christmas cracking! 🎅🏻
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
