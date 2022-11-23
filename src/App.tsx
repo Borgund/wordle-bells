b1b22c1b821b63821353c27c074438ded0c585c2
Sivert Randen <sivert.randen@dnb.no>
2022-11-23 11:48:35 +0100

    Setup keyboard listener
    Add basic state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import dnbLogo from "./assets/dnb-logo.png";
-import "./App.css";
-import Snowfall from "react-snowfall";
-import Countdown from "react-countdown";
-import CustomCountdown from "./components/countdown";
-import FancyDay from "./components/fancyDay";
+import React, { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import dnbLogo from './assets/dnb-logo.png';
+import './App.css';
+import Snowfall from 'react-snowfall';
+import Countdown from 'react-countdown';
+import CustomCountdown from './components/countdown';
+import FancyDay from './components/fancyDay';
 
 function App() {
+  const [attempts, setAttempts] = useState(['', '', '', '', '', '']);
+  const [activeGuess, setActiveGuess] = useState('');
+  const [activeIndex, setActiveIndex] = useState(0);
   const today = new Date();
-  const christmas = new Date("2022-12-24");
+  const christmas = new Date('2022-12-24');
   const isBeforeChristmas = +today < +christmas;
+
+  const saveAttempt = () => {
+    if (activeGuess.length === 5)
+      setAttempts((prevAttempts) => {
+        const newAttempts = [...prevAttempts];
+        newAttempts[activeIndex] = activeGuess;
+        return newAttempts;
+      });
+    if (activeIndex < 6) {
+      setActiveIndex((prevIndex) => prevIndex + 1);
+    }
+    setActiveGuess('');
+  };
+
+  const handleKeyDown = ({ key }: KeyboardEvent) => {
+    const letterRegex = /[a-zA-Z]/;
+    if (key.length === 1 && letterRegex.test(key)) {
+      if (activeGuess.length < 5) {
+        setActiveGuess((prevGuess) => prevGuess + key.toUpperCase());
+      }
+    }
+    if (key === 'Enter') {
+      saveAttempt();
+    }
+    if (key === 'Backspace') {
+      setActiveGuess((prevGuess) => prevGuess.slice(0, -1));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  });
 
   return (
     <div className="App">
@@ -35,8 +71,11 @@ function App() {
           <p>It's Christmas baby! Go do something else! 🎅🏻</p>
         </CustomCountdown>
       </p>
-
-      <Snowfall style={{ zIndex: "-100" }} snowflakeCount={40} />
+      <p>{activeGuess}</p>
+      {attempts.map((attempt) => (
+        <div key="attempt">{attempt}</div>
+      ))}
+      <Snowfall style={{ zIndex: '-100' }} snowflakeCount={40} />
     </div>
   );
 }
