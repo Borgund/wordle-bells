1c6c204c97a08f2d4e8836b8d34d6a4130b1514f
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2023-11-30 19:43:08 +0100

    Minor improvements

--- src/components/north-star/NorthStar.tsx
+++ src/components/north-star/NorthStar.tsx
@@ -1,78 +1,72 @@
+import { useEffect, useState } from "react";
 import star from "../../assets/north-star.svg";
 import pentagram from "../../assets/pentagram.svg";
 import styles from "./NorthStar.module.scss";
 import { client } from "../../pocketbase";
-import { useEffect, useState } from "react";
 
 export const NorthStar = () => {
+  const [satanMode, setSatanMode] = useState(false);
 
-  async function isSatanMode() {
-
-    var lastJesusAttempt = "";
-    var lastSatanAttempt = "";
-  
-    const satan = await client.collection('attempts').getFirstListItem('attempt="SATAN"', {
-      sort: '-created',
-    }).then((e) => {
-      lastSatanAttempt = e.created;
-    }).catch((e) => {
-      console.log(e);
-    });
-  
-    const jesus = await client.collection('attempts').getFirstListItem('attempt="JESUS"', {
-      sort: '-created',
-    }).then((e) => {
-      lastJesusAttempt = e.created;
-    }).catch((e) => {
-      console.log(e);
-    });
-    console.log("ATTEMPTS")
-    console.log(lastJesusAttempt);
-    console.log(lastSatanAttempt);
-  
-  
-    // Apply satan mode
-    if (lastJesusAttempt === "" && lastSatanAttempt !== "") {
-      setSatanMode(true)
-    } else if (lastJesusAttempt !== "" && lastSatanAttempt === "") {
-      setSatanMode(false)
-    } else {
-      // Both are defined
-      var jesusDate = new Date(lastJesusAttempt);
-      var satanDate = new Date(lastSatanAttempt);
-  
-      if (satanDate > jesusDate) {
-        setSatanMode(true)
-      } else {
-        setSatanMode(false)
-      }
+  const getLastAttempt = async (type: string) => {
+    try {
+      const result = await client
+        .collection("attempts")
+        .getFirstListItem(`attempt="${type}"`, {
+          sort: "-created",
+        });
+      return result.created;
+    } catch (error) {
+      console.log(error);
+      return "";
     }
-  }
+  };
 
-  const [satanMode, setSatanMode] = useState(false)
+  const isSatanMode = async () => {
+    const [lastJesusAttempt, lastSatanAttempt] = await Promise.all([
+      getLastAttempt("JESUS"),
+      getLastAttempt("SATAN"),
+    ]);
+    const satanCheck =
+      lastJesusAttempt === ""
+        ? lastSatanAttempt !== ""
+        : new Date(lastSatanAttempt) > new Date(lastJesusAttempt);
+    setSatanMode(satanCheck);
+  };
 
-  useEffect(() => {  
-    isSatanMode()
-    client.collection('attempts').subscribe('*', isSatanMode);
+  useEffect(() => {
+    isSatanMode();
+    const unsubscribe = client
+      .collection("attempts")
+      .subscribe("*", isSatanMode);
 
+    return () => {
+      client.collection("attempts").unsubscribe("*");
+    };
   }, []);
 
-  if (satanMode) {
-    console.log("Applying satan mode")
-    // @ts-ignore
-    document.body.style = `
-    --color-x-mas-blue-sky: var(--color-red-dark);
-    filter: blur(2px) contrast(7);
-    transform: scale(0.9) rotate(5deg);
+  useEffect(() => {
+    const bodyStyle = satanMode
+      ? `
+        --color-x-mas-blue-sky: var(--color-red-dark);
+        filter: blur(0.1px) contrast(1.4);
+        transform: scale(0.9) rotate(3deg);
+      `
+      : "";
 
-    `
-  } else {
-    console.log("Applying jesus mode")
     // @ts-ignore
-    document.body.style = ""
-  }
+    document.body.style = bodyStyle;
+
+    return () => {
+      // @ts-ignore
+      document.body.style = "";
+    };
+  }, [satanMode]);
 
   return (
-    <img className={styles.star} src={satanMode ? pentagram : star} alt="yellow shaped star"></img>
+    <img
+      className={styles.star}
+      src={satanMode ? pentagram : star}
+      alt="yellow shaped star"
+    />
   );
 };
